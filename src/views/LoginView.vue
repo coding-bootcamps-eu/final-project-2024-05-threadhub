9cf067d4419c6838ae99ea87dbfc76984203a7f7
Mario Ramirez <mario-ramirez@hotmail.de>
2024-08-07 09:46:10 +0200

    add: footer

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -4,15 +4,17 @@
       <img src="/src/assets/images/logos.png" width="350px" alt="threadhublogo" />
     </header>
     <div><LoginViewContainer /></div>
+    <footer><FooterComponent /></footer>
   </div>
 </template>
 
 <script>
 import LoginViewContainer from '@/components/LoginViewContainer.vue';
-
+import FooterComponent from '@/components/FooterComponent.vue';
 export default {
   components: {
     LoginViewContainer,
+    FooterComponent,
   },
 };
 </script>
