439fd0112361349bba8c1610cc9fb1f8cdf8269e
Mario Ramirez <mario-ramirez@hotmail.de>
2024-08-07 11:43:33 +0200

    add: Container in HomeView

---
+++ src/components/PostListContainer.vue
@@ -0,0 +1,48 @@
+<template>
+  <section class="post-list-container">
+    <div v-for="post in posts" :key="post.id" class="post">
+      <h3>{{ post.title }}</h3>
+      <p>{{ post.subtitle }}</p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      posts: [
+        {
+          id: 1,
+          title: 'Max09922',
+          subtitle: 'heute ist wieder Mittwoch',
+        },
+        {
+          id: 2,
+          title: 'Torsten099',
+          subtitle: 'Super Tag heute!',
+        },
+        {
+          id: 3,
+          title: 'Mario78773',
+          subtitle: 'Hallo World!',
+        },
+      ],
+    };
+  },
+};
+</script>
+<style scoped>
+.post-list-container {
+  padding: 1.5rem;
+  background-color: var(--header-color);
+  border-radius: 1.3rem;
+}
+.post {
+  background-color: var(--background-inputfield);
+  border: 1px solid #ccc;
+  padding: 0.4rem;
+  margin-bottom: 1rem;
+  border-radius: 0.5rem;
+}
+</style>
