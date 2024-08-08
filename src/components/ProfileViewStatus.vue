4619aafc703b52486d1213c2c1733dd4d00ebeb7
Torsten Michalek <tor.mic.bootcamp@gmail.com>
2024-08-08 10:54:28 +0200

    first version of the   ProfileViewStatus component

---
+++ src/components/ProfileViewStatus.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+    <section class="profile-status-container">
+      <div class="profile-status">
+        <div>Likes:</div>
+        <div>35</div>
+      </div>
+      <div class="profile-status">
+        <div>Posts:</div>
+        <div>5</div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  margin-top: 2rem;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-content: center;
+}
+
+.profile-status-container {
+  background-color: var(--header-color);
+  height: 4rem;
+  width: 80%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  border-radius: 10px;
+}
+
+.profile-status > div {
+  color: var(--font-color);
+  padding: 0.25rem 0.25rem;
+}
+
+.profile-status {
+  display: flex;
+  flex-direction: row;
+  padding-left: 1rem;
+}
+</style>
