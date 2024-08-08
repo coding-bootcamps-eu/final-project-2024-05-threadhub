d5fa6480eee4ffba2e7f4af073631d07bdadc8a3
Max Noelp <noelpmax@gmail.com>
2024-08-08 10:44:35 +0200

    add: View Seite für Post und Registriert im Router

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,6 @@ import HomeView from '@/views/HomeView.vue';
 import LoginView from '@/views/LoginView.vue';
 import ProfileView from '@/views/ProfileView.vue';
 
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -31,6 +30,11 @@ const router = createRouter({
       path: '/login',
       name: 'loginview',
       component: LoginView,
+    },
+    {
+      path: '/post',
+      name: 'postview',
+      component: () => import('@/views/PostView.vue'),
     },
   ],
 });
