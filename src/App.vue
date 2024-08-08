e9dc29635341ca7143688d74772bdc6f510adf8a
Torsten Michalek <tor.mic.bootcamp@gmail.com>
2024-08-08 15:18:58 +0200

    style: fixes footer in all view components and inserts to the app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,29 @@
 <template>
-
   <router-view />
+  <footer>
+    <FooterComponent />
+  </footer>
 </template>
 
+<script>
+import FooterComponent from '@/components/FooterComponent.vue';
 
+export default {
+  components: {
+    FooterComponent,
+  },
+};
+</script>
 
+<style>
+html,
+body,
+#app {
+  height: 100%;
+}
+
+#app {
+  display: grid;
+  grid-template-rows: 1fr auto;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,23 +3,23 @@
     <header><HomeViewHeader /></header>
     <div class="filter"><ProfileViewFilter /></div>
     <PostListContainer />
-    <footer><FooterComponent /></footer>
   </div>
 </template>
+
 <script>
 import HomeViewHeader from '@/components/HomeViewHeader.vue';
-import FooterComponent from '@/components/FooterComponent.vue';
 import ProfileViewFilter from '@/components/ProfileViewFilter.vue';
 import PostListContainer from '@/components/PostListContainer.vue';
+
 export default {
-  components: { ProfileViewFilter, FooterComponent, HomeViewHeader, PostListContainer },
+  components: { ProfileViewFilter, HomeViewHeader, PostListContainer },
 };
 </script>
+
 <style scoped>
 #home {
   display: flex;
   flex-direction: column;
-  height: 100vh;
 }
 
 .filter {
