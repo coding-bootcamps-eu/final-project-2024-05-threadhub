4167ee2740725be410860638df7c576957e42a8c
Max Noelp <noelpmax@gmail.com>
2024-08-12 11:23:09 +0200

    add Button & Up/DOwnVotes

--- src/components/InteraktionPostView.vue
+++ src/components/InteraktionPostView.vue
@@ -1,14 +1,28 @@
 <template>
   <div class="interaktion">
     <div class="votes-thread">
-      <p>🠕<span>23</span></p>
-      <p>🠗<span>23</span></p>
+      <div class="votes" @click="counter++">
+        <p>🠕</p>
+        <p>{{ counter }}</p>
+      </div>
+
+      <div class="votes" @click="down++">
+        <p>🠗</p>
+        <p>{{ down }}</p>
+      </div>
     </div>
     <button @click="showInput">Senf dazugeben</button>
   </div>
   <div class="comment-section" v-show="showComment">
-    <p>Hier kannst du deinen Senf dazugeben...</p>
-    <input type="text" name="text" id="comment-input" /><label for="comment-input"></label>
+    <textarea
+      v-model="userInput"
+      maxlength="2000"
+      type="text"
+      name="text"
+      id="comment-input"
+      placeholder="Hier kannst du deinen Senf dazugeben..."
+    /><label for="comment-input"></label>
+    <p>{{ maxCharLength() }}/{{ charLength }}</p>
   </div>
 </template>
 <script>
@@ -16,11 +30,18 @@ export default {
   data() {
     return {
       showComment: false,
+      counter: 0,
+      down: 0,
+      charLength: 2000,
+      userInput: '',
     };
   },
   methods: {
     showInput() {
       this.showComment = !this.showComment;
+    },
+    maxCharLength() {
+      return this.charLength - this.userInput.length;
     },
   },
 };
@@ -34,6 +55,17 @@ export default {
   font-size: 1rem;
 }
 
+.votes {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 0.5rem;
+}
+
+.votes:active {
+  color: black;
+}
+
 .interaktion {
   display: flex;
   flex-direction: row;
@@ -44,11 +76,38 @@ export default {
 
 .interaktion Button {
   width: 6rem;
-  height: 2rem;
+  height: 1.75rem;
   font-size: 0.68rem;
   color: var(--font-color);
   border: none;
   border-radius: 10px;
   background-color: var(--header-color);
+}
+
+.comment-section {
+  display: flex;
+  flex-direction: column;
+  margin: 0 1rem 0 1rem;
+  padding: 1rem 1rem 0 1rem;
+  background-color: var(--font-color);
+  border-radius: 8px;
+}
+
+.comment-section textarea {
+  text-align: left;
+
+  line-height: 1rem;
+
+  height: 6rem;
+  overflow-y: auto;
+
+  border: None;
+  border-radius: 8px;
+  box-shadow: inset 0px 0px 5px 0px rgba(0, 0, 0, 0.74);
+  padding: 0.5rem;
+}
+
+.comment-section p {
+  text-align: right;
 }
 </style>
